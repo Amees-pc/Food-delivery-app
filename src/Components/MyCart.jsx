d65f0eee60b32ff5bfa921dfdde349de5bd0bb9a
Amees-pc <lgspc2912@gmail.com>
2026-09-22 11:56:21 +0530

    mycart almost completed

--- src/Components/MyCart.jsx
+++ src/Components/MyCart.jsx
@@ -10,6 +10,11 @@ import Tabakh from "../assets/assets/Tabakh.webp";
 import GF from "../assets/assets/Globalfusion.jpg";
 import Arabian from "../assets/chickenTikka.png";
 import Sweet from "../assets/assets/Sweet.webp";
+import {ShoppingCart, Trash2 , MapPin,CreditCard, Wallet, Search, Home, ChevronRight,Truck, ShieldCheck,Leaf, Headphones} from "lucide-react";
+import { FaMotorcycle,
+  FaHeadset,
+} from "react-icons/fa";
+
 const MyCart = () => {
     const [payment,setpayment]=useState("")
     const [items,setitems]=useState({i1:1,i2:1,i3:1,i4:1})
@@ -54,20 +59,23 @@ const MyCart = () => {
     }
     
   return (
-    <div className=' mx-30 mt-6 '> 
-  <div >  
-    <div className='font-bold text-3xl'>Your Cart</div>
-    <div className='text-gray-500 text-sm'>3 items in your cart</div>
+    <div>
+
+<div className=' mx-30 mt-6 '> 
+  <div className='flex gap-4'>
+    <ShoppingCart size={53} className='text-red-500'/>  
+    <div><div className='font-bold text-3xl'>Your Cart</div>
+    <div className='text-gray-500 text-sm'>3 items in your cart</div></div>
   </div>
-  <div className='flex gap-5 mt-6'>
-    <div className='rounded-xl shadow-md '>
+  <div className='flex gap-6 mt-6'>
+    <div className='rounded-xl p-2 shadow-[0_0_15px_rgba(0,0,0,0.3)] '>
         {/* 1 */}
         <div className='p-2 border-b border-gray-200 flex justify-between w-2xl'>
 
             <div className='flex gap-3 w-60'>
                 <img src="https://images.unsplash.com/photo-1579871494447-9811cf80d66c" className='rounded-xl h-20 w-25 object-fit' alt="" />
                 <div>
-                <div className=' text-lg font-bold '>Sushi</div>
+                <div className=' text-xl font-bold '>Sushi</div>
                 <div className='text-sm text-gray-500'>Global Fussion</div>
                 <div className='text-red-400'>$2.00</div>
                 </div>
@@ -83,7 +91,7 @@ const MyCart = () => {
 
  <div className='flex justify-center items-center text-red-600 pr-6'>
     
- X
+<Trash2 size={25}/>
  </div>
              </div>
 
@@ -93,7 +101,7 @@ const MyCart = () => {
             <div className='flex gap-3 w-60'>
                 <img src={Arabian} className='rounded-xl h-20 w-25 object-fit' alt="" />
                 <div>
-                <div className=' text-lg font-bold '> Grill Chicken</div>
+                <div className=' text-xl font-bold '> Grill Chicken</div>
                 <div className='text-sm text-gray-500'>Global Fussion</div>
                 <div className='text-red-400'>$2.00</div>
                 </div>
@@ -109,7 +117,7 @@ const MyCart = () => {
 
  <div className='flex justify-center items-center text-red-600 pr-6'>
     
- X
+ <Trash2 size={25}/>
  </div>
              </div>
 {/* 3 */}
@@ -134,7 +142,7 @@ const MyCart = () => {
 
  <div className='flex justify-center items-center text-red-600 pr-6'>
     
- X
+ <Trash2 size={25}/>
  </div>
              </div>
 
@@ -160,16 +168,16 @@ const MyCart = () => {
 
  <div className='flex justify-center items-center text-red-600 pr-6'>
     
- X
+<Trash2 size={25}/>
  </div>
              </div>
 
     </div>
-    <div className='rounded-xl p-4 shadow-md w-[30vw]'>
+    <div className='rounded-xl p-4 shadow-[0_0_15px_rgba(0,0,0,0.3)] w-[25vw]'>
         <div className='text-xl font-bold '>Order Summary</div>
         <div className='py-4 border-b border-gray-300'>
             <div className='flex justify-between my-4 mt-2'>
-            <div className='text-gray-500'>Subtotal (4 items)</div>
+            <div className='text-gray-500'>Subtotal</div>
             <div className='text-gray-500'>${cost}</div>
         </div>
          <div className='flex justify-between my-4'>
@@ -185,16 +193,21 @@ const MyCart = () => {
         <div className='font-bold text-xl'>Total</div>
         <div className='text-xl font-bold text-red-700'>${1.60 +cost}</div>
        </div>
-       <button className='w-full bg-red-500 text-white py-1 mt-5 rounded-md'>Place Order</button>
+       <button className='w-full bg-red-500 text-white py-1 mt-5 rounded-md flex justify-center'>Place Order <ChevronRight size={25}/></button>
 
     </div>
   </div>
 
-  <div className='flex gap-4 mt-6'>
-<div className='rounded-xl p-3 shadow-md '>
-<div className='font-bold text-lg pb-1'>Delivery  Address</div>
-<div className=' rounded-md'>
-<div className='p-3 flex gap-3'>
+  <div className='flex gap-6 mt-6'>
+<div className='rounded-xl p-3 shadow-[0_0_15px_rgba(0,0,0,0.3)] '>
+<div className=' pb-1 flex gap-3'>
+    <div className='text-red-600'><MapPin size={23}/></div>
+    <div className='font-bold text-lg'>Delivery Address</div>
+</div>
+<div className=' rounded-md flex'>
+    <div className=' flex pt-3'><Home size={27} className='text-red-600'/></div>
+<div>
+    <div className='p-3 flex gap-3'>
     <div className='font-bold text-lg'>Home</div>
     <div className='bg-red-300 text-red-800 px-2 rounded-xl text-sm justify-center flex items-center'>Default</div>
 </div>
@@ -204,36 +217,90 @@ const MyCart = () => {
 </div>
 </div>
 </div>
+</div>
 
-<div className='rounded-xl shadow-md p-3 w-85'>
-    <div className='font-bold text-lg pb-1'>Payment Method</div>
+<div className='rounded-xl shadow-[0_0_15px_rgba(0,0,0,0.3)] p-3 w-85'>
+    <div className='pb-1 flex gap-3'>
+        <div className='pt-0.5 text-red-600'>
+            <CreditCard size={23}/>
+        </div>
+        <div className='font-bold text-lg'>Payment Method</div>
+    </div>
     <div className='rounded-xl'>
 
-        <div className=' border-b border-gray-300'>
-       <button className='flex' onClick={()=>{setpayment("cod")}}><div className={`p-2 font-bold ${payment=="cod"?"text-red-600":""}`}>O</div>
-       <div className='p-2'>
-        <div className='font-bold text-sm'>Cash on Delivery</div>
-       </div></button>
-        </div>
-
-        <div className=' border-b border-gray-300'>
-       <button className='flex' onClick={()=>{setpayment("upi")}}><div className={`p-2 font-bold ${payment=="upi"?"text-red-600":""}`}>O</div>
-       <div className='p-2'>
-        <div className='font-bold text-sm'>UPI</div>
-       </div></button>
-        </div>
-
-         <div className=' border-b border-gray-300'>
+        <div className=' border-b border-gray-300  flex justify-between'>
        <button className='flex' onClick={()=>{setpayment("card")}}><div className={`p-2 font-bold ${payment=="card"?"text-red-600":""}`}>O</div>
-       <div className='p-2'>
+       <div className='p-2 '>
         <div className='font-bold text-sm'>Credit / Debit Card</div>
-       </div></button>
+       </div>
+       </button>
+       <div className=' pt-2 text-red-600'> <Wallet size={22}/></div>
+        </div>
+
+         <div className=' border-b border-gray-300  flex justify-between'>
+       <button className='flex' onClick={()=>{setpayment("card")}}><div className={`p-2 font-bold ${payment=="card"?"text-red-600":""}`}>O</div>
+       <div className='p-2 '>
+        <div className='font-bold text-sm'>UPI</div>
+       </div>
+       </button>
+       {/* <div className=' pt-2'> <CreditCard size={22}/></div> */}
+        </div>
+
+         <div className=' border-b border-gray-300  flex justify-between'>
+       <button className='flex' onClick={()=>{setpayment("card")}}><div className={`p-2 font-bold ${payment=="card"?"text-red-600":""}`}>O</div>
+       <div className='p-2 '>
+        <div className='font-bold text-sm'>Credit / Debit Card</div>
+       </div>
+       </button>
+       <div className='text-red-600 pt-2'> <CreditCard size={22}/></div>
         </div>
 
     </div>
     </div>
 
 </div>
+    </div>
+<div className='mt-6 flex gap-30 bg-pink-50 px-30 pt-3 pb-6'>
+    {/* 1 */}
+    <div className='flex gap-3'>
+     <div className='text-red-500 pt-2'><FaMotorcycle size={45}/></div>
+     <div>
+        <div className='text-xl font-bold'>Fast Delivery</div>
+        <div className='text-gray-500'>On time, every time</div>
+     </div>
+    </div>
+
+    {/* 2 */}
+
+    <div className='flex gap-3'>
+     <div className='text-red-500 pt-2'><ShieldCheck size={45}/></div>
+     <div>
+        <div className='text-xl font-bold'>Safe & Secure </div>
+        <div className='text-gray-500'>100% secure payments</div>
+     </div>
+    </div>
+
+    {/* 3 */}
+
+    <div className='flex gap-3'>
+     <div className='text-red-500 pt-2'><Leaf size={45}/></div>
+     <div>
+        <div className='text-xl font-bold'>Fresh & Hygenic </div>
+        <div className='text-gray-500'>Quality food, always</div>
+     </div>
+    </div>
+
+    {/* 4 */}
+ 
+ <div className='flex gap-3'>
+     <div className='text-red-500 pt-2'><FaHeadset size={45}/></div>
+     <div>
+        <div className='text-xl font-bold'>24/7 Support </div>
+        <div className='text-gray-500'>We're here to help</div>
+     </div>
+    </div>
+
+ </div>
     </div>
   )
 }
